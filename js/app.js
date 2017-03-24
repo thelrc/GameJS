782f17cb41a9ff19fb1271d28a699136088d8cc2
Manuel Banuelos <manueldbanuelos@gmail.com>
2017-03-23 17:51:45 -0700

    Changed original 9 buttons to 4 in html, deleted extra styles in css, added button in html and eventlistener on click in js

--- js/app.js
+++ js/app.js
@@ -1,1 +1,8 @@
-console.log('Suhhh Dude');
+// #Score is id for span of Score
+
+var gameOn;
+
+document.querySelector("#startbtn").addEventListener('click', function () {
+    gameOn = true;
+});
+
